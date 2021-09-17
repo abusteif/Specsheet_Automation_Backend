c57520d5eb983f1e4a5e1af51bbda49c50aee298
d763231 <mustapha.assoum@hotmail.com>
2021-09-17 10:27:32 +1000

    before big CHOICE changes and after small ones

--- helpers/UECI_spec_info_extraction_helpers.py
+++ helpers/UECI_spec_info_extraction_helpers.py
@@ -67,20 +67,23 @@ def find_sequence(line_index, lines):
                     line_to_check.strip().split(" ")[0] == "lateNonCriticalExtension" or "Following" in line_to_check:
                 continue
             if "::=" in line_to_check.split()[1]:
-                continue
+                if "CHOICE" not in line_to_check:
+                    continue
             if "[[" in line_to_check:
                 line_to_check = line_to_check[2:].strip()
                 of_value = handle_of(line_to_check)
 
             if ("SEQUENCE" in line_to_check or "CHOICE" in line_to_check) and not of_value and line_num != 0:
-                dict_sequence[line_to_check.strip().split(" ")[0]] = (find_sequence(line_num + line_index, lines), False)
+                dict_sequence[line_to_check.strip().split(" ")[0]] = (find_sequence(line_num + line_index, lines), "CHOICE" in line_to_check)
                 skip_line = True
                 continue
             if ("SEQUENCE" in line_to_check or "CHOICE" in line_to_check) and line_num == 0:
                 continue
 
             if "SEQUENCE" in line_to_check and of_value:
-                dict_sequence[of_value[0]] = ({of_value[1]: (of_value[1], False)}, True)
+                dict_sequence[of_value[0]] = ({of_value[1]: (of_value[1],
+                                                             check_for_subsequent_exceptions(lines, of_value[1] +
+                                                                                             " ")["list"])}, True)
                 continue
 
             if of_value:
@@ -95,7 +98,6 @@ def find_sequence(line_index, lines):
             return {lines[line_index].strip().split(" ")[0]: (lines[line_index].strip().split(" ")[2], False)}
 
 def check_for_subsequent_exceptions(lines, keyword):
-
     result = {
         "list": False,
         "end": ""
@@ -103,15 +105,17 @@ def check_for_subsequent_exceptions(lines, keyword):
 
     for line_n, line in enumerate(lines):
         if line.startswith(keyword):
-            r = find_sequence(line_n, lines)
-
-            if isinstance(r, dict) and list(r.keys()).__len__() == 1 and r[list(r.keys())[0]][0] == list(r.keys())[0]:
+            if "CHOICE" in line:
                 result["list"] = True
+            else:
+                r = find_sequence(line_n, lines)
 
-            if "SEQUENCE" not in line:
-                if isinstance(r, dict) and list(r.keys()).__len__() == 1 and r[list(r.keys())[0]][0] == "BIT":
-                    # print(keyword)
-                    result["end"] = "BIT"
+                if isinstance(r, dict) and list(r.keys()).__len__() == 1 and r[list(r.keys())[0]][0] == list(r.keys())[0]:
+                    result["list"] = True
+
+                if "SEQUENCE" not in line:
+                    if isinstance(r, dict) and list(r.keys()).__len__() == 1 and r[list(r.keys())[0]][0] == "BIT":
+                        result["end"] = "BIT"
     return result
 
 
@@ -180,3 +184,18 @@ def simplify_last_child_lists(sequence):
             simplify_last_child_lists(element_to_check)
     return sequence
 
+def make_wireshark_related_adjustments_helper(sequence):
+    for s in sequence:
+        element_to_check = sequence[s]
+        if isinstance(element_to_check, tuple):
+            element_to_check = element_to_check[0]
+        if not isinstance(element_to_check, dict):
+            continue
+        for k in list(element_to_check.keys()):
+            if k[-2:] == "Id" and k[-3] != "-":
+                sequence[s] = ("INTEGER", sequence[s][1])
+            if k == "FreqBandIndicatorEUTRA":
+                if s == "bandEUTRA":
+                    sequence[s] = ("INTEGER", sequence[s][1])
+
+        make_wireshark_related_adjustments_helper(element_to_check)
